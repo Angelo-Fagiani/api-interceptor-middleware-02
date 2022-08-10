bfdc24e330bc7ca3bcc60ea361249abf45a97aec
Angelo <angelofag3095@gmail.com>
2022-08-10 08:43:50 +0200

    Aggiunta dei commenti per Carlo

--- src/main/java/it/develhope/apiinterceptormiddleware02/interceptor/MonthInterceptor.java
+++ src/main/java/it/develhope/apiinterceptormiddleware02/interceptor/MonthInterceptor.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+
 @Component
 public class MonthInterceptor implements HandlerInterceptor {
     private static List<Month> monthList = new ArrayList<>(Arrays.asList(
@@ -19,6 +20,10 @@ public class MonthInterceptor implements HandlerInterceptor {
             new Month(3,"March","Marzo","Marsch"),
             new Month(4,"April","Aprile","April")
     ));
+
+    //-Questo metodono l'ho un pò scopiazzato , non riesco a capire secondo quale criterio vengano impostati i return con TRUE o FALSE
+    //potresti spiegarmi?
+    //-Che utilità ha il "request.setAttribute"?
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String monthNumberString = request.getHeader("monthNumber");
         if (monthNumberString == null || monthNumberString.isEmpty()) {
@@ -31,8 +36,7 @@ public class MonthInterceptor implements HandlerInterceptor {
             return SingleMonth.getMonthNumber() == monthNumber;
         }).findAny();
         if (month.isPresent()) {
-
-            request.setAttribute("MonthInterceptor-month", month.get());
+                request.setAttribute("MonthInterceptor-month", month.get());
         } else {
             response.setStatus(400);
         }
